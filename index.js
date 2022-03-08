f532de00c443cc929399ae04d7b1cf47c639de4c
Juan Toro <jct20xx@gmail.com>
2022-03-08 00:42:18 -0500

    commit tree fc61bc42410bf8fe4926bb7c75ab2e67918875e3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+const event = 'suprise';
+
+function writeCards(cards, event) {
+    for (let i = 0; i < cards.length; i++) {
+        cards[i] = `Thank you, ${cards[i]}, for the wonderful ${event} gift!`
+    }
+    return cards;
+}
+
+function countDown(number){
+    let i = number;
+    while (i >= 0) {
+        console.log (i);
+        i--;
+    }
+    return number;
+}
